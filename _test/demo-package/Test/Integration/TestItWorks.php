4bc0854cfcc6f527c678f8e712f79500524efde6
Damien Retzinger <damienwebdev@gmail.com>
2022-06-25 12:14:49 -0400

    feat: add Installation Test Action (#1)

---
+++ _test/demo-package/Test/Integration/TestItWorks.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Graycore\DemoPackage\Test\Integration;
+
+class TestItWorks extends \PHPUnit\Framework\TestCase
+{
+
+    public function testItWorks()
+    {
+        $this->assertEquals(true, true);
+    }
+}
