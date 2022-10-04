346992e92610a55eaae6930367a19a85d0e43705
Damien Retzinger <damienwebdev@gmail.com>
2022-10-04 12:30:36 -0400

    alpha version, because why not

--- supported-version/src/nightly/get-next-version.ts
+++ supported-version/src/nightly/get-next-version.ts
@@ -15,7 +15,7 @@ export const getNextVersion = (repository: Repository, date: Date) => {
     switch(repository){
         case "https://upstream-mirror.mage-os.org":
             // See: https://github.com/mage-os/generate-mirror-repo-js/blob/bbbdf1708ea0bf8fc845aad8240d00f37632b4a7/src/release-branch-build-tools.js#L71
-            return `*`;
+            return "@alpha";
         default:
             return "";
     }
